1c340c6b77985183f4a29fa7b305c306f13e8406
alice <wotupset@gmail.com>
2018-07-26 22:45:28 +0800

    Add files via upload

---
+++ 180728/dll/animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animals {
+    public abstract class animal {
+        private String type;
+        private String name;
+        private String color;
+        public String Type { get; set; }
+        public String Name { get; set; }
+        public String Color { get; set; }
+        public animal(String type,String name,String color) {
+            this.Type = type;
+            this.Name = name;
+            this.Color = color;
+        }
+        public override abstract string ToString();
+            //{return String.Format("來自anime {0},{1},{2}", Type, Name, Color);}
+    }
+}
