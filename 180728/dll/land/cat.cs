cbfea861dcb395f2a880e76e2d86aa46989e4b75
alice <wotupset@gmail.com>
2018-07-26 22:46:23 +0800

    Add files via upload

---
+++ 180728/dll/land/cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animals.land {
+    public class CAT : animal {
+        public CAT(String name, String color) : base("貓",name, color) {
+
+        }
+        public override string ToString() {
+            return String.Format("來自CAT {0},{1},{2}",Type, Name, Color);
+        }
+    }
+}
---
+++ 180728/dll/land/dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animals.land {
+    public class Dog:animal {
+        public Dog(String name,String color) : base("狗", name,color) {
+
+        }
+        public override string ToString() {
+            return String.Format("來自dog {0},{1},{2}",Type,Name,Color); 
+        }
+    }
+}
